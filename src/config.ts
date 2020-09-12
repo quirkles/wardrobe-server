f926def8c145c00942ffcc91292fe09e89c943de
quirkles <al.quirk@gmail.com>
2020-09-12 16:24:20 -0400

    ts fix

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,11 @@ if (process.env.APP_ENV && process.env.APP_ENV === 'dev') {
     }
 }
 
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
-export const ENCRYPTION_IV = process.env.ENCRYPTION_IV;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_PORT = process.env.DB_PORT;
-export const DB_USERNAME = process.env.DB_USERNAME;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_DATABASE = process.env.DB_DATABASE;
+export const JWT_SECRET = process.env.JWT_SECRET || '';
+export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || '';
+export const ENCRYPTION_IV = process.env.ENCRYPTION_IV || '';
+export const DB_HOST = process.env.DB_HOST || '';
+export const DB_PORT = process.env.DB_PORT || '';
+export const DB_USERNAME = process.env.DB_USERNAME || '';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_DATABASE = process.env.DB_DATABASE || '';
