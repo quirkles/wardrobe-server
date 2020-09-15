cc8ccff9352ce44c0b69718b7f52ece2471a46c3
quirkles <al.quirk@gmail.com>
2020-09-14 22:06:53 -0400

    Add healthcheck

---
+++ src/resolvers/HealthCheckResolver.ts
@@ -0,0 +1,12 @@
+import { Resolver, Query } from 'type-graphql';
+
+import { Service } from 'typedi';
+
+@Resolver()
+@Service()
+export class HealthCheckResolver {
+    @Query(() => String)
+    healthCheck(): string {
+        return 'ok!';
+    }
+}
